1ea267687ff9f82373a341bce2427c9584a02df3
chandan <chandanchethan321@gmail.com>
2024-06-13 18:18:40 +0530

    fixed colors

--- src/app/dashboard/[client]/research/page.tsx
+++ src/app/dashboard/[client]/research/page.tsx
@@ -7,16 +7,16 @@ import { Icons } from "@/components/ui/Icons";
 const page = () => {
   return (
     <div className="grid w-full grid-cols-[1fr_300px] overflow-hidden p-5 gap-5 h-[85vh]">
-      <div className="w-full bg-[#F0F2F5] rounded-[30px] p-5 flex flex-col justify-between h-[inherit] py-10">
-        <div className="h-[90%]">
+      <div className="w-full bg-[#DDDDDD] rounded-[30px] p-5 flex flex-col justify-between h-[83vh] py-10 bg-opacity-25">
+        <div className="h-[90%] ">
           <ScrollArea className="h-full flex flex-col w-full">
-            <div className="bg-[#E8EAE8] rounded-[30px] p-5 max-w-[40%] mb-5 self-start">
+            <div className="rounded-[30px] p-5 max-w-[40%] mb-5 self-start">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
               corporis aliquid similique velit explicabo soluta aut maxime ipsa
               sunt ea quaerat eaque, molestias reprehenderit qui! Dicta beatae
               porro iste veritatis!
             </div>
-            <div className="bg-[#E8EAE8] rounded-[30px] p-5 max-w-[40%] mb-5 self-end">
+            <div className="bg-[#CDCDCD] bg-opacity-25 rounded-[30px] p-5 max-w-[40%] mb-5 self-end">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
               corporis aliquid similique velit explicabo soluta aut maxime ipsa
               sunt ea quaerat eaque, molestias reprehenderit qui! Dicta beatae
@@ -24,19 +24,19 @@ const page = () => {
               adipisicing elit. Ullam corporis aliquid similique velit explicabo
               soluta aut maxime ipsa sunt ea quaerat eaque, molestias
             </div>
-            <div className="bg-[#E8EAE8] rounded-[30px] p-5 max-w-[40%] mb-5 self-start">
+            <div className=" rounded-[30px] p-5 max-w-[40%] mb-5 self-start">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
               corporis aliquid similique velit explicabo soluta aut maxime ipsa
               sunt ea quaerat eaque, molestias reprehenderit qui! Dicta beatae
               porro iste veritatis!
             </div>
-            <div className="bg-[#E8EAE8] rounded-[30px] p-5 max-w-[40%] mb-5 self-end">
+            <div className="bg-[#CDCDCD] bg-opacity-25 rounded-[30px] p-5 max-w-[40%] mb-5 self-end">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
               corporis aliquid similique velit explicabo soluta aut maxime ipsa
               sunt ea quaerat eaque, molestias reprehenderit qui! Dicta beatae
               porro iste veritatis!
             </div>
-            <div className="bg-[#E8EAE8] rounded-[30px] p-5 max-w-[40%] mb-5 self-start">
+            <div className="rounded-[30px] p-5 max-w-[40%] mb-5 self-start">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
               corporis aliquid similique velit explicabo soluta aut maxime ipsa
               sunt ea quaerat eaque, molestias reprehenderit qui! Dicta beatae
@@ -44,7 +44,7 @@ const page = () => {
               adipisicing elit. Ullam corporis aliquid similique velit explicabo
               soluta aut maxime ipsa sunt ea quaerat eaque, molestias
             </div>
-            <div className="bg-[#E8EAE8] rounded-[30px] p-5 max-w-[40%] mb-5 self-end">
+            <div className="bg-[#CDCDCD] bg-opacity-25 rounded-[30px] p-5 max-w-[40%] mb-5 self-end">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
               corporis aliquid similique velit explicabo soluta aut maxime ipsa
               sunt ea quaerat eaque, molestias reprehenderit qui! Dicta beatae
@@ -53,10 +53,10 @@ const page = () => {
           </ScrollArea>
         </div>
         <div>
-          <div className="flex gap-5 bg-white rounded-[30px] p-5">
+          <div className="flex gap-5 bg-white rounded-[30px] p-3">
             <Input
               type="text"
-              className="outline-none shadow-none border-none font-montserrat"
+              className="outline-none shadow-none border-none font-montserrat outline-none focus:border-none"
               placeholder="Type a message"
             />
             <Button
@@ -68,21 +68,21 @@ const page = () => {
           </div>
         </div>
       </div>
-      <div className="w-full bg-[#F0F2F5] rounded-[30px] p-5 flex flex-col">
-        <div className="bg-[#E8EAE8] rounded-[30px] p-5  mb-5 self-start">
+      <div className="w-full bg-[#DDDDDD] rounded-[30px] p-5 flex flex-col h-[83vh] bg-opacity-25">
+        <div className="bg-[#CDCDCD] rounded-[30px] p-5  mb-5 self-start bg-opacity-25">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
           corporis aliquid similique velit explicabo soluta aut maxime ipsa sunt
         </div>
 
-        <div className="bg-[#E8EAE8] rounded-[30px] p-5  mb-5 self-start">
+        <div className="bg-[#CDCDCD] rounded-[30px] p-5  mb-5 self-start bg-opacity-25">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
           corporis aliquid similique velit explicabo soluta aut maxime ipsa sunt
         </div>
-        <div className="bg-[#E8EAE8] rounded-[30px] p-5  mb-5 self-start">
+        <div className="bg-[#CDCDCD] rounded-[30px] p-5  mb-5 self-start bg-opacity-25">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
           corporis aliquid similique velit explicabo soluta aut maxime ipsa sunt
         </div>
-        <div className="bg-[#E8EAE8] rounded-[30px] p-5  mb-5 self-start">
+        <div className="bg-[#CDCDCD] rounded-[30px] p-5  mb-5 self-start bg-opacity-25">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
           corporis aliquid similique velit explicabo soluta aut maxime ipsa sunt
         </div>
