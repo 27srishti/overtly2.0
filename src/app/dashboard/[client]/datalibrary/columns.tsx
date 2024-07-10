2f5636ce54304d5f02c35236e12eb30fbe7a15ff
chandan <chandanchethan321@gmail.com>
2024-07-10 22:23:26 +0530

    table parts ready

---
+++ src/app/dashboard/[client]/datalibrary/columns.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string;
+  url: string;
+  name: string;
+  originalName: string;
+  type: string;
+  createdAt: string;
+  bucketName: string;
+  filesCategory: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "type",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Type
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+];
---
+++ src/app/dashboard/[client]/datalibrary/data-table.tsx
@@ -0,0 +1,223 @@
+"use client";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  SortingState,
+  getSortedRowModel,
+  PaginationState,
+} from "@tanstack/react-table";
+import * as React from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  pageCount: number;
+  defaultPerPage?: number;
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  pageCount,
+  defaultPerPage = 2,
+}: DataTableProps<TData, TValue>) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  // Search params
+  const page = parseInt(searchParams.get("page") as string, 10) || 1;
+  const perPage =
+    parseInt(searchParams.get("per_page") as string, 2) || defaultPerPage;
+  const sort = searchParams.get("sort") || "";
+  const [column, order] = sort.split(".") ?? [];
+
+  const createQueryString = React.useCallback(
+    (params: Record<string, string | number | null>) => {
+      const newSearchParams = new URLSearchParams(searchParams?.toString());
+
+      for (const [key, value] of Object.entries(params)) {
+        if (value === null) {
+          newSearchParams.delete(key);
+        } else {
+          newSearchParams.set(key, String(value));
+        }
+      }
+
+      return newSearchParams.toString();
+    },
+    [searchParams]
+  );
+
+  const [{ pageIndex, pageSize }, setPagination] =
+    React.useState<PaginationState>({
+      pageIndex: page - 1,
+      pageSize: perPage,
+    });
+
+  const pagination = React.useMemo(
+    () => ({
+      pageIndex,
+      pageSize,
+    }),
+    [pageIndex, pageSize]
+  );
+
+  const [sorting, setSorting] = React.useState<SortingState>([
+    {
+      id: column ?? "",
+      desc: order === "desc",
+    },
+  ]);
+
+  const table = useReactTable({
+    data: data.slice(pageIndex * pageSize, (pageIndex + 1) * pageSize),
+    columns,
+    pageCount,
+    state: {
+      pagination,
+      sorting,
+    },
+    manualPagination: true,
+    manualSorting: true,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onPaginationChange: setPagination,
+    onSortingChange: setSorting,
+  });
+
+  React.useEffect(() => {
+    router.push(
+      `${pathname}?${createQueryString({
+        page: pageIndex + 1,
+        per_page: pageSize,
+        sort: sorting[0]?.id
+          ? `${sorting[0].id}.${sorting[0].desc ? "desc" : "asc"}`
+          : null,
+      })}`,
+      { scroll: false }
+    );
+  }, [pageIndex, pageSize, sorting, createQueryString, pathname, router]);
+
+  const handleChangeRowsPerPage = (newPerPage: number) => {
+    setPagination({ pageIndex: 0, pageSize: newPerPage });
+  };
+
+  return (
+    <div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-between space-x-2 py-4">
+        <div className="flex items-center space-x-2">
+          <span className="text-sm">Rows per page:</span>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => handleChangeRowsPerPage(10)}
+            disabled={pageSize === 10}
+          >
+            10
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => handleChangeRowsPerPage(20)}
+            disabled={pageSize === 20}
+          >
+            20
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => handleChangeRowsPerPage(50)}
+            disabled={pageSize === 50}
+          >
+            50
+          </Button>
+        </div>
+        <div>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
