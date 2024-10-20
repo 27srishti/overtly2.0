70ad28b89e041dd0eff5ab4c7ff7a78ae33356c1
chandan <chandanchethan321@gmail.com>
2024-10-20 10:18:25 +0530

    fixed eslint

--- src/app/dashboard/[client]/research/page.tsx
+++ src/app/dashboard/[client]/research/page.tsx
@@ -158,7 +158,6 @@ const Page = () => {
             </PopoverTrigger>
             <PopoverContent className="w-auto p-0" align="start">
               <Calendar
-                initialFocus
                 mode="range"
                 defaultMonth={date?.from}
                 selected={date}
